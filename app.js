804e80cb377ad2e8d7fa161b1c1068751a777d3d
Ivan <isergey87@mail.ru>
2021-12-07 23:58:24 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// function printText(name, age) {
+//     console.log("Hello! My name is: " +name+ ", Age: " +age);
+
+// }
+
+// printText("Sergey", 34);
+// printText("Toma", 32);
+// printText("David", 5);
+
+
+
+// const human = {
+//     name: 'Sergey',
+//     age: 34,
+//     cat: 'Asya',
+//     hello() {
+//         console.log('Меня зовут: ' + this.name);
+//         console.log('Мне ' + this.age + ' года');
+//         console.log('Мою кошку зовут: ' + this.cat);
+//     }
+// }
+// console.log(human.hello());
+
+// let arr = [1, 5, 2, 25, 16, 14, 49, 3, 1, 7];
+// // arr.sort(function(a, b) {
+// //     // if (a==b) return 0 
+// //     // if (a>b) return 1
+// //     // if (a<b) return -1
+
+// //     return a-b
+// // })
+
+// arr.sort((a,b) => a - b);
+
+// console.log(arr);
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>OOP
+
+
+let user = {
+    name: 'Sergey',
+    age: 20,
+}
+
+
